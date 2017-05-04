0bf5114101308b07daa35fa514e200803acb1230
DH-TB <929659475@qq.com>
2017-05-04 16:02:52 +0800

    modify readme and function test

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 npm install
 npm test
 ```
+这个demo包括对数据库的测试，以及简单功能测试，还有express server的测试
--- mocha/mocha.js
+++ mocha/mocha.js
@@ -12,6 +12,7 @@ describe('two-Array',()=>{
     describe('#indexof()',()=>{
         it('should return -1 when the value of not found',()=>{
             assert.equal(-1,[1,2,3].indexOf(4));
+            expect(-1).to.equal([1,2,3].indexOf(4));
         })
     })
 });
